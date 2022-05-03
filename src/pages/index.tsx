04aa30442b53282b3454f5909628f443e93fd0e5
Wellington <wellington.map@hotmail.com>
2022-05-03 08:44:38 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>hello world</h1>
+    </>
+  )
+}
+
+export default Home
